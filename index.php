65b2d700a5eb6733ff2c00a9a27b051d111f3972
ErickDC <erickdiazc717@gmail.com>
2023-08-13 20:18:41 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hola";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio Web - Libros</title>
+</head>
+
+<body>
+    Sitio Web
+</body>
+
+</html>
