d325c573caa7480ab02a8351bcacea3f0083235a
ErickDC <erickdiazc717@gmail.com>
2023-08-13 20:32:38 -0500

    navbar added

--- index.php
+++ index.php
@@ -14,7 +14,16 @@ echo "Hola";
 </head>
 
 <body>
-    Sitio Web
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="#" aria-current="page">Nav 1 <span class="visually-hidden">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nav 2</a>
+            </li>
+        </ul>
+    </nav>
 </body>
 
 </html>
