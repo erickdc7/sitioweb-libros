70ee816ced39ac399e86d24395397603e283a5e9
ErickDC <erickdiazc717@gmail.com>
2023-08-13 20:40:59 -0500

    jumbotron added

--- index.php
+++ index.php
@@ -29,7 +29,13 @@
 
     <div class="container">
         <div class="row">
-
+            <div class="p-5 mb-4 bg-light rounded-3">
+                <div class="container-fluid py-5">
+                    <h1 class="display-5 fw-bold">Custom jumbotron</h1>
+                    <p class="col-md-8 fs-4">Texto</p>
+                    <button class="btn btn-primary btn-lg" type="button">Example button</button>
+                </div>
+            </div>
         </div>
     </div>
 </body>
