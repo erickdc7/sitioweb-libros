c7e4ec74fcf6cb85bdf3b1aed2d5be2c62098af0
ErickDC <erickdiazc717@gmail.com>
2023-08-15 10:53:06 -0500

    estructura html con bootstrap agregado

--- administrador/index.php
+++ administrador/index.php
@@ -1,14 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+ <!DOCTYPE html>
+ <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administrador del Sitio Web</title>
-</head>
+ <head>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<body>
-    Login
-</body>
+     <!-- Bootstrap CSS v5.2.1 -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
-</html>
+     <title>Administrador de Sitio Web</title>
+ </head>
+
+ <body>
+     Login
+ </body>
+
+ </html>
