f342f5bf71d37f9974b3436e2094343b5f620b72
ErickDC <erickdiazc717@gmail.com>
2023-08-15 11:11:38 -0500

    class cambiado

--- administrador/index.php
+++ administrador/index.php
@@ -12,7 +12,7 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-4">
                 <div class="card">
                     <div class="card-header">
                         Login
