5a77c9f60a91ed537d5d356a837abc7e68c57e07
ErickDC <erickdiazc717@gmail.com>
2023-08-15 11:03:24 -0500

    form agregado

--- administrador/index.php
+++ administrador/index.php
@@ -21,7 +21,18 @@
                         Login
                     </div>
                     <div class="card-body">
-
+                        <form>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Usuario</label>
+                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Entrar al sistema</button>
+                        </form>
                     </div>
                 </div>
             </div>
