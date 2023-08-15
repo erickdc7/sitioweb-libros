ab277d01ed0f60a6cea5f169fa8ccd0a60205620
ErickDC <erickdiazc717@gmail.com>
2023-08-15 11:04:59 -0500

    name agregado

--- administrador/index.php
+++ administrador/index.php
@@ -29,7 +29,7 @@
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
+                                <input type="password" class="form-control" name="contrasenia" id="exampleInputPassword1">
                             </div>
                             <button type="submit" class="btn btn-primary">Entrar al sistema</button>
                         </form>
