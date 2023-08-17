e110492cb3e84b18271998d20c42737fc3c8dc44
ErickDC <erickdiazc717@gmail.com>
2023-08-17 10:37:19 -0500

    form cambiado

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -6,30 +6,27 @@
             Datos del Libro
         </div>
         <div class="card-body">
-            <h4 class="card-title">Title</h4>
-            <p class="card-text">Text</p>
+            <form method="POST" enctype="multipart/form-data">
+                <div class="mb-3">
+                    <label class="form-label" for="txtID">ID:</label>
+                    <input type="text" class="form-control" id="txtID" name="txtID" placeholder="ID">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="txtNombre">Nombre:</label>
+                    <input type="text" class="form-control" id="txtNombre" name="txtNombre" placeholder="Nombre del libro">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="txtNombre">Imagen:</label>
+                    <input type="file" class="form-control" id="txtNombre" name="txtNombre" placeholder="Nombre del libro">
+                </div>
+                <div class="btn-group" role="group" aria-label="Button group name">
+                    <button type="button" class="btn btn-success">Agregar</button>
+                    <button type="button" class="btn btn-warning">Modificar</button>
+                    <button type="button" class="btn btn-info">Cancelar</button>
+                </div>
+            </form>
         </div>
     </div>
-
-    <form method="POST" enctype="multipart/form-data">
-        <div class="mb-3">
-            <label class="form-label" for="txtID">ID:</label>
-            <input type="text" class="form-control" id="txtID" name="txtID" placeholder="ID">
-        </div>
-        <div class="mb-3">
-            <label class="form-label" for="txtNombre">Nombre:</label>
-            <input type="text" class="form-control" id="txtNombre" name="txtNombre" placeholder="Nombre del libro">
-        </div>
-        <div class="mb-3">
-            <label class="form-label" for="txtNombre">Imagen:</label>
-            <input type="file" class="form-control" id="txtNombre" name="txtNombre" placeholder="Nombre del libro">
-        </div>
-        <div class="btn-group" role="group" aria-label="Button group name">
-            <button type="button" class="btn btn-success">Agregar</button>
-            <button type="button" class="btn btn-warning">Modificar</button>
-            <button type="button" class="btn btn-info">Cancelar</button>
-        </div>
-    </form>
 </div>
 
 <div class="col-md-7">
