e83b79a708ceb5ea0e8f8a82949747cc94886152
ErickDC <erickdiazc717@gmail.com>
2023-08-17 10:31:08 -0500

    input agregado

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -10,6 +10,10 @@
             <label class="form-label" for="txtNombre">Nombre:</label>
             <input type="text" class="form-control" id="txtNombre" name="txtNombre" placeholder="Nombre del libro">
         </div>
+        <div class="mb-3">
+            <label class="form-label" for="txtNombre">Nombre:</label>
+            <input type="text" class="form-control" id="txtNombre" name="txtNombre" placeholder="Nombre del libro">
+        </div>
         <button type="submit" class="btn btn-primary">Entrar al Administrador</button>
     </form>
 </div>
